0c5b73e47831c8836794b79c52b64337ff538e1a
night-sornram <sornram.chalernma@gmail.com>
2023-12-13 16:08:34 +0700

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,69 @@
+"use client"
 import Image from 'next/image'
+import { useState } from 'react'
+
+const valid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 export default function Home() {
+  const [firstName , setFirstName ] = useState("")
+  const [lastName , setLastName ] = useState("")
+  const [email , setEmail ] = useState("")
+  const [password , setPassword ] = useState("")
+  const [showfirstName , setShowFirstName ] = useState(false)
+  const [showlastName , setShowLastName ] = useState(false)
+  const [showemail , setShowEmail ] = useState(false)
+  const [showpassword , setShowPassword ] = useState(false)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className=' w-screen h-screen flex flex-col md:flex-row'>
+      <div className='py-20 md:py-0 w-full md:w-1/2 flex justify-center items-center'>
+        <div className=' flex flex-col w-4/6'>
+          <div className=' text-center md:text-start text-5xl text-white font-bold'>
+            Learn to code by watching others
+          </div>
+          <div className='  text-center md:text-start  text-white mt-10'>
+            See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.
+          </div>
+        </div>
+      </div>
+      <div className=' w-full md:w-1/2 flex justify-center pb-20 md:pb-0 md:justify-start items-center'>
+        <div className=' flex flex-col w-11/12 lg:w-10/12 xl:w-8/12'>
+          <div className=' bg-custom-200 p-5 rounded-lg text-center text-white shadow-[0_8px_0px_0px_rgba(0,0,0,0.2)]'>
+            <span className=' font-bold'>Try it free 7 days</span> then $20/mo. thereafter
+          </div>
+          <div className=' bg-white p-7 mt-10 rounded-lg flex space-y-5 flex-col shadow-[0_8px_0px_0px_rgba(0,0,0,0.2)]'>
+            <div className=' flex flex-col'>
+              <input type="text" placeholder={ showfirstName ? "" : "First Name"}  value={firstName} onChange={(e)=>{setFirstName(e.currentTarget.value)}} className={ showfirstName ? "e" : "i"} />
+              <div className={ showfirstName ? "s" : "u"}>First Name cannot be empty</div>
+            </div>
+            <div className=' flex flex-col'>
+              <input type="text" placeholder={ showlastName ? "" : "Last Name"} value={lastName} onChange={(e)=>{setLastName(e.currentTarget.value)}} className={ showlastName ? "e" : "i"}/>
+              <div className={ showlastName ? "s" : "u"}>Last Name cannot be empty</div>
+            </div>
+            <div className=' flex flex-col'>
+              <input type="email" placeholder={ showemail ? "" : 'Email Address'}  value={email} onChange={(e)=>{setEmail(e.currentTarget.value)}} className={ showemail ? "e" : "i"}/>
+              <div className={ showemail ? "s" : "u"}>Looks like this is not an email</div>
+            </div>
+            <div className=' flex flex-col'>
+              <input type="password" placeholder={ showpassword ? "" : 'Password'} value={password} onChange={(e)=>{setPassword(e.currentTarget.value)}} className={ showpassword ? "e" : "i"}/>
+              <div className={ showpassword ? "s" : "u"}>Password cannot be empty</div>
+            </div>
+            <button  
+            onClick={()=>{
+              firstName === "" ? setShowFirstName(true) : setShowFirstName(false);
+              lastName === "" ? setShowLastName(true) : setShowLastName(false);
+              !email.match(valid) ? setShowEmail(true) : setShowEmail(false);
+              password === "" ? setShowPassword(true) : setShowPassword(false);
+            }}
+            
+            className=' uppercase bg-custom-100 p-5 rounded-lg text-white font-medium'>
+              Claim your free trial
+            </button>
+            <div className=' text-xs text-center text-gray-400'>
+              By clicking the button, you are agreeing to our <span className=' text-custom-500'>Terms and Services</span>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+    </div>
+  )}
